d277e2e7c74d65ad5c73606e8890dbc562398966
royvardhan <42067944+royvardhan@users.noreply.github.com>
2022-01-24 20:44:41 +0530

    Deployed a DAO in the Ethereum Rinkeby network
    Contract has voting functions to facilitate NFT purchases.

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x263498208d7397f4DDDF0Ec17E700d5B9d7bF33C";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
